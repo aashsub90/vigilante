85a5784b37397feae8bfbb79b0e5bdcf23c81d18
Chintan Vachhani <chintan.vachhani10@gmail.com>
2018-05-07 01:15:54 -0700

    Implemented evaluation graph to notebooks.

--- slackapp/app.py
+++ slackapp/app.py
@@ -127,7 +127,7 @@ def loadModel():
 	"""
 		Loads final model
 	"""
-	with open('model.pkl', 'rb') as f:
+	with open('randomForest_model.pkl', 'rb') as f:
 		model = pickle.load(f)
 	return (model)
 
